dc892ed7ca00744bda4d06be64e82431901bc8a2
kiss810529-lang <kiss810529@gmail.com>
2026-09-23 14:05:01 +0800

    Connect website to Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,8 @@
-export const firebaseConfig={apiKey:"REPLACE_WITH_FIREBASE_API_KEY",authDomain:"REPLACE_WITH_PROJECT.firebaseapp.com",projectId:"REPLACE_WITH_PROJECT_ID",storageBucket:"REPLACE_WITH_PROJECT.firebasestorage.app",messagingSenderId:"REPLACE_WITH_SENDER_ID",appId:"REPLACE_WITH_APP_ID"};
+export const firebaseConfig = {
+  apiKey: "AIzaSyBN_emOzmXkUhH6rCwxyo9EGSsiSVguhTk",
+  authDomain: "puzzle-order.firebaseapp.com",
+  projectId: "puzzle-order",
+  storageBucket: "puzzle-order.firebasestorage.app",
+  messagingSenderId: "967279987178",
+  appId: "1:967279987178:web:2bc4c12304b4d57c35ca93"
+};
